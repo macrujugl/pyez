bffbc57cb0521cdd88f14b2d5df912a97983f096
macrujugl <marienza@es.ibm.com>
2015-09-21 15:32:42 +0200

    New exe for bgp_table.yml

--- bgp_neig_table.py
+++ bgp_neig_table.py
@@ -1,6 +1,5 @@
 
-m jnpr.junos.factory import loadyaml
-from jnpr.junos.op import *
+from jnpr.junos.factory import loadyaml
 from jnpr.junos import Device
 
 globals().update( loadyaml('/root/pyez/bgp_table.yml'))
@@ -8,3 +7,5 @@ dev = Device(host='172.2.3.5',user='mario',ssh_private_key_file='/root/.ssh/VRF'
 dev.open()
 tbl=BgpNeigh(dev)
 tbl.get()
+for k in tbl:
+   print k.items()
